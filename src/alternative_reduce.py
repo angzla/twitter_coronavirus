0deec2ba8f0f5403df13e3a8256248b8bc86d72c
Angela <angzla329@gmail.com>
2023-05-01 21:15:01 -0700

    fixing alternative png again

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -36,7 +36,7 @@ dates = [0, 60, 120, 180, 240, 300, 360]
 months = ['Jan.', "March", "May", "July", "Sept.", "Nov."]
 plt.xticks(dates, months)
 plt.xlabel('Date')
-plt.title("Tweets with Coronavirus Hashtags per Day in 2020")
+plt.title("Hashtags used in Tweets per Day in 2020")
 plt.legend()
 plt.tight_layout()
 plt.savefig('alternative')
