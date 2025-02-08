9c92e98f8f1ff5873bdf5e251e4083307a61007c
@angzla <angzla329@gmail.com>
2025-02-08 14:46:39 -0800

    png

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -1,62 +1,50 @@
 #!/usr/bin/env python3
 
-# command line args
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_paths', nargs='+', required=True)
-parser.add_argument('--keys', nargs='+', required=True)
+
+parser.add_argument('--hashtags',nargs='+',required=True)
 args = parser.parse_args()
-print(args.keys)
 
-# imports
-import json
-print("import json done")
-from collections import Counter, defaultdict
-print("import collections done")
 import matplotlib
-print("matplotlib done")
 matplotlib.use('Agg')
+import os
+import re
+import json
+from collections import Counter,defaultdict
 import matplotlib.pyplot as plt
-print("matplotlib.pyplot")
-import numpy as np
-print("numpy done")
-print("imports done")
-for key in args.keys:
-    sorted(args.input_paths)
-    yaxis = [0] * 366
-    total = defaultdict(lambda: Counter())
-    errors = 0
-for path in args.input_paths:
-    with open(path) as f:
-        temp = json.load(f)
-        try:
-            for k, v in temp[key].items():
-                if k.isdigit():
-                    day = int(k)  # Ensure `k` is a valid day index
-                    if 0 <= day < 366:
-                        yaxis[day] += v  # Aggregate tweets per day
-        except KeyError:
-            pass
 
+total = {}
 
-#for path in args.input_paths:
- #       with open(path) as f:
-  #          temp = json.load(f)
-   #         number = 0
-    #        try:
-     #           for k in temp[key]:
-      #              number += temp[key][k]
-       #     except KeyError:
-        #        pass
-         #   yaxis.append(number)
-plt.plot(np.arange(366), yaxis, label=key)
-plt.ylabel('Number of Tweets')
-months = ["Jan.", "Feb.", "March", "April", "May", "June"]
-dates = [0, 31, 60, 91, 121, 152]  # Approximate month start days in 2020
-plt.xticks(dates, months)
+for hashtag in args.hashtags:
+    dayCount = {}
+    for filename in sorted(os.listdir('outputs')):
+        dayTweets = os.path.join('outputs', filename)
+        date = filename[10:18]
+        if os.path.isfile(dayTweets) and 'lang' in dayTweets:
+            tweetCount = 0
+            with open(dayTweets) as file:
+                data = json.load(file)
+                for k in data:
+                    if hashtag == k:
+                        for key in data[k]:
+                            tweetCount += data[k][key]
+            dayCount[date] = tweetCount
+    total[hashtag] = dayCount
+
+plt.figure(figsize=(14, 8))
+for hashtag, dates_counts in total.items():
+    dates_sorted = sorted(dates_counts.keys())
+    counts = [dates_counts[date] for date in dates_sorted]
+    plt.plot(dates_sorted, counts, label=hashtag)
+
 plt.xlabel('Date')
-plt.title("Hashtags used in Tweets per Day Jan-July in 2020")
+plt.ylabel('Tweet Count')
+plt.title('Tweet Counts by Hashtag Over Time')
+all_dates = list(sorted(total[next(iter(total))].keys()))
+selected_dates = all_dates[::20]
+plt.xticks(selected_dates, rotation=45)
 plt.legend()
 plt.tight_layout()
-plt.savefig('alternative')
+plt.savefig('hashtag_trends.png')
 plt.close()
